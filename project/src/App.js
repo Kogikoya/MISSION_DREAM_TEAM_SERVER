5d3f57e84cf5ceddeaa4a9c30b09828338180565
kogikoya <kogizoahae@gmail.com>
2024-06-11 10:10:29 +0900

    .

--- project/src/App.js
+++ project/src/App.js
@@ -30,7 +30,7 @@ function App() {
   let navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('http://www.missiondreamteam.kro.kr/api/CheckLoginState.php')
+    axios.get('http://localhost/MISSION_DREAM_TEAM/PHP/CheckLoginState.php')
     .then(res => {
       if(res.data === false){
         setIsLoggedIn(false);
@@ -46,7 +46,7 @@ function App() {
   useEffect(() => {
     const fetchUserInfo = async () => {
       try {
-        const res = await axios.get('http://www.missiondreamteam.kro.kr/api/GetInfo.php');
+        const res = await axios.get('http://localhost/MISSION_DREAM_TEAM/PHP/GetInfo.php');
         const userData = res.data;
         setUserName(userData.name);
         const missionCnt = userData.totalMissionCnt - userData.noMissionCnt
@@ -60,7 +60,7 @@ function App() {
 
     const fetchProfileImage = async () => {
       try {
-        const res = await axios.get('http://www.missiondreamteam.kro.kr/api/ProfileImageShow.php');
+        const res = await axios.get('http://localhost/MISSION_DREAM_TEAM/PHP/ProfileImageShow.php');
         let originalPath = res.data.profilePath;
         if (originalPath != null) {
           if (originalPath === '/img/default_profile.png') {
@@ -94,7 +94,7 @@ function App() {
         return; // 미션 입력란이 비어 있으면 함수 종료
       }
       // 새로운 미션 추가
-      const res = await axios.post('http://www.missiondreamteam.kro.kr/api/Insert_mission.php', {
+      const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/Insert_mission.php', {
         mission: missionInput // 미션 내용
       });
     
@@ -129,7 +129,7 @@ function App() {
                   <h6>today { point }</h6>
                   <img className="imgs" onClick={() => { navigate('/updateinfo') }} src="/img/gear.png"/>
                   <button className="button-logout" onClick={()=>{
-                    axios.post('http://www.missiondreamteam.kro.kr/api/LogOut.php')
+                    axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/LogOut.php')
                     .then(res => {
                       navigate('/login')
                     })
@@ -161,7 +161,7 @@ function App() {
                 </Nav>
               </div>
               {
-                tap == 0 ? <ToDo setCreate={setCreate} setJoin={setJoin} groupList={groupList} missionList={missionList} setMissionList={setMissionList} setGroupList={setGroupList} navigate={navigate}/> : null
+                tap == 0 ? <ToDo setCreate={setCreate} setJoin={setJoin} groupList={groupList} missionList={missionList} setMissionList={setMissionList} setGroupList={setGroupList} navigate={navigate} userName={userName} point={point} profileImage={profileImage}/> : null
               }
               {
                 tap == 1 ? <MyCalendar/> : null
@@ -172,12 +172,12 @@ function App() {
           <Route path="*" element={<Navigate to="/login" />} />
         )}
         <Route path="/signup" element={ <SignUp/> }/>
-        <Route path="/group/*" element={ <Group/> }/>
+        <Route path="/group/*" element={ <Group userName={userName} point={point} profileImage={profileImage}/> }/>
         <Route path="/updateinfo" element={ <UpdateInfo/> }/>
         <Route path="*" element={<div>404</div>}/>
       </Routes>
       <CreateGroup create={create} setCreate={setCreate} setGroupList={setGroupList}/>
-      <JoinGroup join={join} setJoin={setJoin} setGroupList={setGroupList}/>
+      <JoinGroup join={join} setJoin={setJoin} groupList={groupList} setGroupList={setGroupList}/>
       <ChangeProfileImage change={change} setChange={setChange} profileImage={profileImage}/>
     </div>
   );
@@ -186,7 +186,7 @@ function App() {
 
 const fetchMissions = async (setMissionList) => {
   try {
-    const res = await axios.get(`http://www.missiondreamteam.kro.kr/api/Show_mission.php?`)
+    const res = await axios.get(`http://localhost/MISSION_DREAM_TEAM/PHP/Show_mission.php?`)
     if (res.data == null) {
       setMissionList([]);
     } else {
@@ -203,7 +203,7 @@ const fetchMissions = async (setMissionList) => {
 
 const fetchGroups = async (setGroupList) => {
   try {
-      const res = await axios.get(`http://www.missiondreamteam.kro.kr/api/ShowGroup.php?`)
+      const res = await axios.get(`http://localhost/MISSION_DREAM_TEAM/PHP/ShowGroup.php?`)
       setGroupList(res.data)
   } catch (error) {
       console.error('Error fetching missions:', error)
@@ -228,7 +228,7 @@ function ToDo(props) {
 
     if (currentHour >= 5 && currentHour < 21) {
       try {
-        const res = await axios.post('http://www.missiondreamteam.kro.kr/api/Delete_mission.php', {
+        const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/Delete_mission.php', {
           mission_idx: props.missionList[i][0]
         })
         fetchMissions(props.setMissionList);
@@ -250,7 +250,7 @@ function ToDo(props) {
       console.log(`${pair[0]}: ${pair[1]}`);
     }
     try {
-      const res = await axios.post('http://www.missiondreamteam.kro.kr/api/MissionImageUpload.php', formData, {
+      const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/MissionImageUpload.php', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -328,7 +328,12 @@ function ToDo(props) {
                       <span className="myGroupCount">{content.groupMemberCnt}</span>
                     </div>
                     <div className="myGroupName" onClick={()=>{
-                      props.navigate('/group', { state: { pageGroupName: content.groupName.group_name } });
+                      props.navigate('/group', { state: {
+                        pageGroupName: content.groupName.group_name,
+                        userName: props.userName,
+                        point: props.point,
+                        profileImage: props.profileImage
+                      } });
                     }}>{ content.groupName.group_name }</div>
                   </div>
                 );
@@ -357,7 +362,7 @@ function MissionPhoto(props) {
   return (
       <Modal show={props.photo} onHide={() => {props.setPhoto(false)}} className="main-modal">
           <Modal.Header closeButton>
-              <Modal.Title>{modalTitle}</Modal.Title>
+              <Modal.Title className='mission-photo-title'>{modalTitle}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
               <img src={props.photoSrc} alt={modalTitle} style={{ width: '100%' }} />
@@ -377,7 +382,7 @@ function MyCalendar() {
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const res = await axios.get('http://www.missiondreamteam.kro.kr/api/GetPersonalRecord.php');
+        const res = await axios.get('http://localhost/MISSION_DREAM_TEAM/PHP/GetPersonalRecord.php');
         if (res.data) {
           const formattedData = res.data.map(entry => ({
             date: entry.date.split(' ')[0],
@@ -544,7 +549,7 @@ function CreateGroup(props) {
     const checkGroupName = async () => {
       if (groupName) {
         try {
-          const response = await axios.post('http://www.missiondreamteam.kro.kr/api/GroupNameCheck.php', { groupName });
+          const response = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/GroupNameCheck.php', { groupName });
           setIsGroupNameUnique(response.data);
         } catch (error) {
           console.log(error);
@@ -583,7 +588,7 @@ function CreateGroup(props) {
     const selectedPrice = parseInt(selectedPriceString.replace(/[^\d]/g, ''), 10);
 
     try {
-      const response = await axios.post('http://www.missiondreamteam.kro.kr/api/CreateGroup.php', {
+      const response = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/CreateGroup.php', {
         group_name: groupName,
         penaltyPerPoint: selectedPrice,
         group_notice: groupNotice,
@@ -678,29 +683,40 @@ function JoinGroup(props) {
     event.preventDefault();
     const nameIsEmpty = checkField('name', '그룹 이름을 입력해주세요.');
     const passwordIsEmpty = checkField('password', '비밀번호를 입력해주세요.');
-  
+    let groupNames = []
+    
+    if (props.groupList) {
+      groupNames = props.groupList.map(group => group.groupName.group_name);
+    }
+    
     if (!nameIsEmpty && !passwordIsEmpty) {
         const inputName = document.getElementById('name').value;
         const inputPw = document.getElementById('password').value;
-  
-        axios.post('http://www.missiondreamteam.kro.kr/api/EnterGroup.php',
-        {
-            group_name: inputName,
-            group_password: inputPw
-        })
-        .then((res)=>{
-            if (res.data == true) {
-                alert('[ '+inputName+' ] 그룹에 가입되었습니다!')
-                props.setJoin(false)
-                fetchGroups(props.setGroupList);
-            }
-            else {
-                alert('그룹 이름 또는 비밀번호를 확인해주세요.')
-            }
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+        
+        if (groupNames.includes(inputName)) {
+          alert('이미 가입된 그룹입니다.')
+          props.setJoin(false)
+        }
+        else {
+          axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/EnterGroup.php',
+          {
+              group_name: inputName,
+              group_password: inputPw
+          })
+          .then((res)=>{
+              if (res.data == true) {
+                  alert('[ '+inputName+' ] 그룹에 가입되었습니다!')
+                  props.setJoin(false)
+                  fetchGroups(props.setGroupList);
+              }
+              else {
+                  alert('그룹 이름 또는 비밀번호를 확인해주세요.')
+              }
+          })
+          .catch((err)=>{
+              console.log(err)
+          })
+        }
     }
   }
   
@@ -756,7 +772,7 @@ function ChangeProfileImage(props) {
     formData.append('imgFile', selectedFile);
 
     try {
-      const res = await axios.post('http://www.missiondreamteam.kro.kr/api/ProfileImageUpload.php', formData,{
+      const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/ProfileImageUpload.php', formData,{
         headers: {
           'Content-Type': 'multipart/form-data',
         }
@@ -781,7 +797,7 @@ function ChangeProfileImage(props) {
     }
 
     try {
-      const res = await axios.post('http://www.missiondreamteam.kro.kr/api/DeleteProfileImage.php');
+      const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/DeleteProfileImage.php');
       if (res.data) {
         alert("프로필 사진이 제거되었습니다!");
         props.setChange(false);
--- project/src/index.js
+++ project/src/index.js
@@ -14,7 +14,6 @@ const persister = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persister}>
         <BrowserRouter>
@@ -22,7 +21,6 @@ root.render(
         </BrowserRouter>
       </PersistGate>
     </Provider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
